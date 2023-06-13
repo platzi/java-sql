193441c2b5a7c731307c61c39fd9c1d6a6ebc7ce
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2023-06-13 13:09:18 -0600

    Update

--- Main.java
+++ Main.java
@@ -5,23 +5,21 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection myConn = null;
-        PreparedStatement myStamt = null;
+        Statement myStamt = null;
+        ResultSet myRes = null;
 
         try{
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "1234.");
             System.out.println("Genial, nos conectamos");
 
-            String sql = "INSERT INTO employees (first_name, pa_surname) VALUES (?, ?)";
+            myStamt = myConn.createStatement();
 
-            myStamt = myConn.prepareStatement(sql);
-            myStamt.setString(1, "Johana");
-            myStamt.setString(2, "Dorantes");
+            int rowsAffected = myStamt.executeUpdate("UPDATE employees " + "set email='johanador2@example.com' " + "WHERE first_name='Johana'");
 
-            int rowsAffected = myStamt.executeUpdate();
- 
-            if(rowsAffected>0){
-                System.out.println("Se ha creado un nuevo cliente");
+            myRes = myStamt.executeQuery("SELECT * FROM employees order by pa_surname");
 
+            while(myRes.next()){
+                System.out.println(myRes.getString("first_name") + "," + myRes.getString("email"));
             }
 
         } catch (Exception e) {
