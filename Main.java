32ce8c861c84217846dec535de054b6df78023ac
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2023-06-13 12:27:26 -0600

    Clase 5, leer insert

--- Main.java
+++ Main.java
@@ -1,19 +1,31 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection myConn = null;
+        PreparedStatement myStamt = null;
 
         try{
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "1234.");
             System.out.println("Genial, nos conectamos");
 
+            String sql = "INSERT INTO employees (first_name, pa_surname) VALUES (?, ?)";
+
+            myStamt = myConn.prepareStatement(sql);
+            myStamt.setString(1, "Johana");
+            myStamt.setString(2, "Dorantes");
+
+            int rowsAffected = myStamt.executeUpdate();
+ 
+            if(rowsAffected>0){
+                System.out.println("Se ha creado un nuevo cliente");
+
+            }
+
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
             System.out.println("Algo salio mal ):");
         }
     }
