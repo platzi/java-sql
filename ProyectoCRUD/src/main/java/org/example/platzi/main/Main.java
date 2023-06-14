7edae75e157357ec636858859b753965ab8116e9
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2023-06-14 10:41:04 -0600

    CRUD Clase 9

--- ProyectoCRUD/src/main/java/org/example/platzi/main/Main.java
+++ ProyectoCRUD/src/main/java/org/example/platzi/main/Main.java
@@ -1,31 +1,25 @@
 package org.example.platzi.main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        Connection myConn = null; // Nuestro objeto de conexion
-        Statement myStamt = null; // Nuestro objeto statement
-        ResultSet myRes = null; // Para procesar nuestros resultados
-        try {
-            // 1. Conexion a nuestra base de datos, pasando 3 argumentos
-            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "1234.");
-            // 2. Crear una declaracion objeto
-            myStamt = myConn.createStatement();
-            // 3. Ejecutar consulta SQL
-            myRes = myStamt.executeQuery("SELECT * FROM employees");
+        String url = "jdbc:mysql://localhost:3306/project";
+        String user = "root";
+        String pass = "1234.";
+
+        try (Connection myConn = DriverManager.getConnection(url,user ,pass );
+             Statement myStamt = myConn.createStatement();
+             ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");){
+
             while (myRes.next()) {
                 System.out.println(myRes.getString("first_name"));
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Algo salio mal ):");
         }
     }
 }
+
